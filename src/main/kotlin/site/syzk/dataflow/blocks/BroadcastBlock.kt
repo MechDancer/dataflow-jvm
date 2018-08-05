0cec4bc05210843da91f59bf1e78f721f973f4e7
YangDR <ydrml@hotmail.com>
2018-08-05 12:59:51 +0800

    删掉一些用不到的设计
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- src/main/kotlin/site/syzk/dataflow/blocks/BroadcastBlock.kt
+++ src/main/kotlin/site/syzk/dataflow/blocks/BroadcastBlock.kt
@@ -3,25 +3,12 @@ package site.syzk.dataflow.blocks
 import site.syzk.dataflow.core.ISource
 import site.syzk.dataflow.core.ITarget
 import site.syzk.dataflow.core.post
-import java.util.concurrent.LinkedBlockingQueue
-import kotlin.concurrent.thread
 
 class BroadcastBlock<T> : ISource<T>, ITarget<T> {
-    private val buffer = LinkedBlockingQueue<T>()
     private val links = mutableListOf<ITarget<T>>()
 
-    init {
-        thread {
-            while (true) {
-                val temp = buffer.take()
-                links.forEach { it.post(temp) }
-            }
-        }
-    }
-
     override fun consume(event: T) {
-        if (!buffer.isEmpty()) buffer.take()
-        buffer.put(event)
+        links.forEach { it.post(event) }
     }
 
     override fun linkTo(target: ITarget<T>) {
--- src/main/kotlin/site/syzk/dataflow/blocks/BufferBlock.kt
+++ src/main/kotlin/site/syzk/dataflow/blocks/BufferBlock.kt
@@ -3,33 +3,28 @@ package site.syzk.dataflow.blocks
 import site.syzk.dataflow.core.ISource
 import site.syzk.dataflow.core.ITarget
 import site.syzk.dataflow.core.post
-import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
 import kotlin.concurrent.thread
 
 class BufferBlock<T> : ISource<T>, ITarget<T> {
-    private val queue = LinkedBlockingQueue<Pair<T, Queue<ITarget<T>>>>()
+    private val queue = LinkedBlockingQueue<T>()
     private val initList = mutableListOf<ITarget<T>>()
 
     init {
         thread {
             while (true) {
-                val current = queue.take()
-                while (current.second.isEmpty()) {
-                }
-                while (current.second.isNotEmpty()) {
-                    current.second.first().post(current.first)
-                }
+                while (initList.isEmpty());
+                while (initList.isNotEmpty())
+                    initList.first().post(queue.take())
             }
         }
     }
 
     override fun linkTo(target: ITarget<T>) {
         initList.add(target)
-        queue.forEach { it.second.offer(target) }
     }
 
     override fun consume(event: T) {
-        queue.put(event to LinkedList(initList))
+        queue.put(event)
     }
 }
