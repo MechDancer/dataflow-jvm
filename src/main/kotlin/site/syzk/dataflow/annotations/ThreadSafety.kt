11dcaa615d6d0768e96972b3b1119b478c0bfade
YangDR <ydrml@hotmail.com>
2018-08-07 14:45:53 +0800

    测试机器人
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- src/main/kotlin/site/syzk/dataflow/annotations/ThreadSafety.kt
+++ src/main/kotlin/site/syzk/dataflow/annotations/ThreadSafety.kt
@@ -3,6 +3,9 @@ package site.syzk.dataflow.annotations
 import kotlin.annotation.AnnotationRetention.SOURCE
 import kotlin.annotation.AnnotationTarget.CLASS
 
+/**
+ * 指示线程安全性
+ */
 @Target(CLASS)
 @Retention(SOURCE)
 annotation class ThreadSafety(val safety: Boolean)
