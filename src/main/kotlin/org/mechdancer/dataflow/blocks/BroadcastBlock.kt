1b1ca97d426bc729cedb468a3c4063f1e530849e
YangDR <ydrml@hotmail.com>
2018-08-24 17:16:26 +0800

    奇怪的编译器bug
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- src/main/kotlin/org/mechdancer/dataflow/blocks/BroadcastBlock.kt
+++ src/main/kotlin/org/mechdancer/dataflow/blocks/BroadcastBlock.kt
@@ -1,9 +1,6 @@
 package org.mechdancer.dataflow.blocks
 
-import org.mechdancer.dataflow.core.DefaultSource
-import org.mechdancer.dataflow.core.IPropagatorBlock
-import org.mechdancer.dataflow.core.IReceivable
-import org.mechdancer.dataflow.core.Link
+import org.mechdancer.dataflow.core.*
 import org.mechdancer.dataflow.core.internal.ReceiveCore
 import org.mechdancer.dataflow.core.internal.SourceCore
 import org.mechdancer.dataflow.core.internal.TargetCore
@@ -15,14 +12,11 @@ import java.util.*
  */
 class BroadcastBlock<T>(
     override val name: String = "broadcast",
-    private val clone: ((T?) -> T)? = null)
+    private val clone: ((T) -> T)? = null)
     : IPropagatorBlock<T, T>, IReceivable<T> {
     override val uuid = UUID.randomUUID()!!
     override val defaultSource by lazy { DefaultSource(this) }
 
-    //--------------------------
-    // IReceivable
-    //--------------------------
     private val receiveCore = ReceiveCore()
     private val sourceCore = SourceCore<T>(1)
     private val targetCore = TargetCore<T> { event ->
@@ -35,6 +29,10 @@ class BroadcastBlock<T>(
     }
 
     override fun offer(id: Long, link: Link<T>) = targetCore.offer(id, link)
-    override fun consume(id: Long): Pair<Boolean, T?> = sourceCore[id]
     override fun receive() = receiveCore getFrom sourceCore
+    override fun consume(id: Long) = sourceCore[id]
+        .let {
+            if (it.hasValue && clone != null) message(clone!!(it.value))
+            else it
+        }
 }
